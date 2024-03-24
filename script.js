97c1b471f800c198a16809f3fbeae40cc13ffc2f
Bruno Paes <bdscp06@outlook.com>
2024-03-24 12:24:46 -0300

    Arrumei a imagem incial

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function toggleMode() {
     const html = document.documentElement
-    html.classList.toggle("light")
+    
+   // if (html.classList.contains('light')) {
+   //     html.classList.remove('light')
+   // } else {
+   //     html.classList.add('light')
+   // }
+   html.classList.toggle("light")
+
 
     // pegar a tag img
     const img = document.querySelector("#profile img")
@@ -8,7 +15,7 @@ function toggleMode() {
  //substituir a imagem
 
 
-    if(html.classList.contains('light')){
+    if(html.classList.contains("light")){
      // se tiver light mode, adicionar a imagem light
         img.setAttribute("src", "./assets/avatar-light.png")
     } else {
